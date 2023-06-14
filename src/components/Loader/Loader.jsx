ccae7baa6981d82fe744295f0f13028c9b65b3bf
raf-coffee <pavel.hexlet@mail.ru>
2023-06-14 21:46:39 +0300

    loader fixes

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,18 +3,18 @@ import styles from "./Loader.module.css";
 export function Loader() {
   return (
     <div className={styles["lds-spinner"]}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
     </div>
   );
 }
