933a7f3d8f91ad39fb6abbfdd680a219ffdc5df8
raf-coffee <pavel.hexlet@mail.ru>
2023-06-17 20:58:02 +0300

    add the mobile version of the body type  page

--- src/components/CountButton/CountButton.jsx
+++ src/components/CountButton/CountButton.jsx
@@ -1,6 +1,6 @@
 export function CountButton({ color = "", disabled = false }) {
   return (
-    <button disabled={disabled} type="submit" className={`btn btn-primary my-md-4  ${color}`}>
+    <button disabled={disabled} type="submit" className={`btn btn-primary mt-md-2 mb-md-4  ${color}`}>
       Расчитать
     </button>
   );
