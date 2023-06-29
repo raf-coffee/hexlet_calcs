55a60da84546166b3a56efb118c43367f4efea56
DmitriySebrekov <admin@ittrue.ru>
2023-06-29 21:24:46 +0300

    NDFL ver2

--- src/calcs/finance/ndfl/ndfl.js
+++ src/calcs/finance/ndfl/ndfl.js
@@ -5,8 +5,45 @@
 // параметр amountPaid - сумма к выплате за вычетом НДФЛ.
 // параметр rateNDFL - процентная ставка НДФЛ (значение по умолчанию 13%)
 
-export const ndfl = (amountPaid, rateNDFL = 13) => {
+//Расчет, если известна сумма после налогообложения. Возвращает массив где первое значение это сумма, а второе НДФЛ 
+const ndflAfter = (amountPaid, rateNDFLnum = 13) => {
+    const rateNDFL = rateNDFLnum;
     const result =  amountPaid / ((100 - rateNDFL) / 100);
-return result.toFixed(2);
+    const ndfl = result * (rateNDFL / 100);
+    //const fullPaid = result;
+    //console.log(fullPaid);
+    /**
+    if (fullPaid > 5000000 ) {
+        const rateNDFLover = 15;
+        const ndfl5m = 650000;
+        const result5m = 4350000;
+        const afterPaidOver = (fullPaid - 5000000) / 0.85; //сумма выше 5 млн.
+        const ndflOver =  afterPaidOver * (rateNDFLover / 100);
+        const resultOver = afterPaidOver - ndflOver;
+        const resultSum = 5000000 + afterPaidOver;
+        const ndflSum = ndflOver +ndfl;
+return {sum:resultSum.toFixed(2), ndflSum:ndflSum.toFixed(2), ndfl:ndfl5m.toFixed(2), afterPaidOver:afterPaidOver.toFixed(2) , ndflover:ndflOver.toFixed(2)};
+    }
+    */
+return {sum:result.toFixed(2), ndfl:ndfl.toFixed(2)};
 };
+//Расчет, если известна сумма до налогообложения (beforePaid - сумма до налога). Возвращает массив где первое значение это сумма, а второе НДФЛ.
+const ndflBefore = (beforePaid, rateNDFLnum = 13) => {
+    const rateNDFL = rateNDFLnum;
+    if (beforePaid > 5000000 ) {
+        const rateNDFLover = 15;
+        const ndfl = 650000;
+        const result = 4350000;
+        const beforePaidOver = beforePaid - 5000000;
+        const ndflOver =  beforePaidOver * (rateNDFLover / 100);
+        const resultOver = beforePaidOver - ndflOver;
+        const resultSum = result + resultOver;
+        const ndflSum = ndflOver +ndfl;
+return {sum:resultSum.toFixed(2), ndflSum:ndflSum.toFixed(2), ndfl:ndfl.toFixed(2), beforePaidOver:beforePaidOver.toFixed(2) , ndflover:ndflOver.toFixed(2)}; 
+    }
+    const ndfl =  beforePaid * (rateNDFL / 100);
+    const result = beforePaid - ndfl;
+return {sum:result.toFixed(2), ndfl:ndfl.toFixed(2)};
+};
+export {ndflAfter, ndflBefore}
 
--- src/calcs/finance/ndfl/ndfl.test.js
+++ src/calcs/finance/ndfl/ndfl.test.js
@@ -1,9 +1,21 @@
 import { test, expect } from 'vitest';
-import { ndfl, nds } from './ndfl';
+import { ndflAfter, ndflBefore, } from './ndfl';
 
-test('NDFL 30%', () => {
-    expect(ndfl(9450.43, 30)).toEqual('13500.61');
+test('NDFL 30% after', () => {
+    expect(ndflAfter(9450.43, 30)).toEqual({sum:'13500.61', ndfl:'4050.18'});
 });
-test('NDFL default', () => {
-    expect(ndfl(9450.43)).toEqual('10862.56');
+test('NDFL default after', () => {
+    expect(ndflAfter(9450.43)).toEqual({sum:'10862.56', ndfl:'1412.13'});
 });
+test('NDFL default before', () => {
+    expect(ndflBefore(24892.40)).toEqual({sum:'21656.39', ndfl:'3236.01'});
+});
+test('NDFL 15% before', () => {
+    expect(ndflBefore(21656.40, 15)).toEqual({sum:'18407.94', ndfl:'3248.46'});
+});
+test('NDFL OVER 5000000 before', () => {
+    expect(ndflBefore(5000100)).toEqual({sum:'4350085.00', ndflSum:'650015.00',  ndfl:'650000.00', beforePaidOver:'100.00' , ndflover:'15.00'});
+});
+//test('NDFL OVER 5000000 after', () => {
+//    expect(ndflAfter(4700000)).toEqual({sum:'5411764.71', ndflSum:'711764.71',  ndfl:'650000.00', afterPaidOver:'411764.71' , ndflover:'61764.71'});
+//});
